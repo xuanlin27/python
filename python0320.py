0c04472a2554a0755ee5c6bb75f022b0508b2f44
linyixuan <d1104181712@gm.lhu.edu.tw>
2024-03-20 11:57:33 +0800

    進度，還在測試有無連到sourcetree

--- python0320.py
+++ python0320.py
@@ -11,3 +11,9 @@ def adjust_image_by_scaling(img, scale=1.0):
     img_adj = img.astype(np.float16)
     img_adj = img_adj * scale
     return img_adj
+
+def convert_image_to_unit8(img):
+    img_adj = np.floor(img_adj + 0.5)
+    img_adj = np.floor(img_adj + 0.5)
+    img_adj[img_adj > 255] = 255
+    img_adj[img_adj < 0 ] = 0   
