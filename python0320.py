9ef5dbed494e50a19dcc011bd6905d7d9ce92afb
linyixuan <d1104181712@gm.lhu.edu.tw>
2024-03-20 11:24:16 +0800

    上課內容

---
+++ python0320.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def adjust_image_by_offset(img, offset=0):
+    img_adj = img.astype(np.float16)
+    img_adj = img_adj + offset
+    return img_adj
+
+def adjust_image_by_scaling(img, scale=1.0):
